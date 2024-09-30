0bce632deb8a013a513869f9bd95cc1e45658c9b
Max <mkorobov2002@mail.ru>
2024-09-30 15:00:42 +0300

    Create SwapIcon.tsx

---
+++ src/components/SwapIcon.tsx
@@ -0,0 +1,21 @@
+export const SwapIcon = () => {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g clipPath='url(#clip0_3_98)'>
+        <path d='M7.99 17H20V19H7.99V22L4 18L7.99 14V17Z' fill='#11B3FE' />
+        <path d='M16.01 8H4V10H16.01V13L20 9L16.01 5V8Z' fill='#11B3FE' />
+      </g>
+      <defs>
+        <clipPath id='clip0_3_98'>
+          <rect width={24} height={24} fill='white' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
