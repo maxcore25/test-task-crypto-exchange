52091b30078b219a2cf6a05eae6eaa0a9b6dab65
Max <mkorobov2002@mail.ru>
2024-09-30 15:00:45 +0300

    Create SwapButton.tsx

---
+++ src/components/SwapButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { SwapIcon } from './SwapIcon';
+
+export type SwapButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const SwapButton = React.forwardRef<HTMLButtonElement, SwapButtonProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          'grid size-6 cursor-pointer place-content-center rounded-full bg-none transition-colors hover:bg-slate-50',
+          className
+        )}
+        ref={ref}
+        {...props}
+      >
+        <SwapIcon />
+      </button>
+    );
+  }
+);
+SwapButton.displayName = 'SwapButton';
