0b7ab50dcb168e3e14ea4c75db81f7afdd4d1b3a
Max <mkorobov2002@mail.ru>
2024-09-30 14:26:52 +0300

    Stylize Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,16 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, ...props }, ref) => {
-    return <button className={cn(className)} ref={ref} {...props} />;
+    return (
+      <button
+        className={cn(
+          'h-[50px] rounded-[5px] bg-brand-color p-[15px] text-base font-bold uppercase leading-tight tracking-wide text-background transition-colors hover:bg-[#0b4c42]',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
 Button.displayName = 'Button';
