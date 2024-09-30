663bed1d0e55703a746fb32a1586d64ee73a64a9
Max <mkorobov2002@mail.ru>
2024-09-30 15:12:51 +0300

    Stylize ExchangeForm

--- src/components/ExchangeForm.tsx
+++ src/components/ExchangeForm.tsx
@@ -1,3 +1,27 @@
+import { Button } from './Button';
+import { Input } from './Input';
+import { SwapButton } from './SwapButton';
+
 export const ExchangeForm = () => {
-  return <div>Enter</div>;
+  return (
+    <form>
+      <div className='mb-12 flex flex-col gap-4 lg:mb-8 lg:flex-row lg:items-center lg:gap-7'>
+        <Input type='number' min={0} name='from' />
+        <SwapButton className='rotate-90 self-end lg:rotate-0 lg:self-auto' />
+        <Input type='number' min={0} name='to' />
+      </div>
+      <div className='grid gap-4 lg:flex lg:items-end lg:gap-8'>
+        <div className='grid w-full gap-2'>
+          <label
+            htmlFor='address'
+            className='text-base font-normal leading-[23px]'
+          >
+            Address
+          </label>
+          <Input type='text' name='address' id='address' />
+        </div>
+        <Button type='submit'>Exchange</Button>
+      </div>
+    </form>
+  );
 };
