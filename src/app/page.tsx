8edbeb7e93b82fa18f0e7b27538b6e8754c52d43
Max <mkorobov2002@mail.ru>
2024-09-30 13:57:57 +0300

    Format HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
-      <div className='grid gap-[14px] lg:gap-4'>
-        <h1 className='text-light-gray text-[40px] leading-[48px] lg:text-[50px] font-light lg:leading-[60px]'>
+      <div className='mb-[60px] grid gap-[14px] lg:gap-4'>
+        <h1 className='text-[40px] font-light leading-[48px] text-light-gray lg:text-[50px] lg:leading-[60px]'>
           Crypto Exchange
         </h1>
-        <div className='text-light-gray text-xl font-normal leading-tight'>
+        <div className='text-xl font-normal leading-tight text-light-gray'>
           Exchange fast and easy
         </div>
       </div>
