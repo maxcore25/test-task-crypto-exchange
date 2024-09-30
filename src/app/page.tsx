ea4f1320ea4ba76f1e9ccae3eea038b9869d52e0
Max <mkorobov2002@mail.ru>
2024-09-30 14:27:05 +0300

    Stylize HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 export default function HomePage() {
   return (
-    <main>
-      <div className='mb-[60px] grid gap-[14px] lg:gap-4'>
-        <h1 className='text-[40px] font-light leading-[48px] text-light-gray lg:text-[50px] lg:leading-[60px]'>
-          Crypto Exchange
-        </h1>
-        <div className='text-xl font-normal leading-tight text-light-gray'>
-          Exchange fast and easy
+    <main className='px-4'>
+      <div className='mx-auto mt-16 max-w-[960px] lg:mt-[220px]'>
+        <div className='mb-[60px] grid gap-[14px] lg:gap-4'>
+          <h1 className='text-[40px] font-light leading-[48px] lg:text-[50px] lg:leading-[60px]'>
+            Crypto Exchange
+          </h1>
+          <div className='text-xl font-normal leading-tight'>
+            Exchange fast and easy
+          </div>
         </div>
       </div>
     </main>
