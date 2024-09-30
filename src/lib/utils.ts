0df47410cbbb3fe3cfee44dcb4111ee2e6658274
Max <mkorobov2002@mail.ru>
2024-09-30 14:36:54 +0300

    Create inputDefaultStyles

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const inputDefaultStyles =
+  'h-[50px] rounded-[5px] border border-[#e3ebef] bg-[#f6f7f8]';
