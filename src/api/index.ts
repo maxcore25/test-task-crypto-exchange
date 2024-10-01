2ef9dd7579c7081e56a1f0410d7d34a726eb10c8
Max <mkorobov2002@mail.ru>
2024-10-01 19:47:29 +0300

    Delete Next.js app from main branch

--- src/api/index.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL: 'https://api.changenow.io/v1',
-});
--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export const inputDefaultStyles =
-  'h-[50px] rounded-[5px] border border-[#e3ebef] bg-[#f6f7f8]';
