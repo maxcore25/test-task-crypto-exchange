e01bf8f122576a843496f44cbd77ccc45bcd79cb
Max <mkorobov2002@mail.ru>
2024-10-01 20:41:51 +0300

    Update SwapIcon.tsx

--- src/components/SwapIcon.tsx
+++ src/components/SwapIcon.tsx
@@ -7,12 +7,12 @@ export const SwapIcon = () => {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <g clipPath='url(#clip0_3_98)'>
-        <path d='M7.99 17H20V19H7.99V22L4 18L7.99 14V17Z' fill='#11B3FE' />
-        <path d='M16.01 8H4V10H16.01V13L20 9L16.01 5V8Z' fill='#11B3FE' />
+      <g clipPath='url(#clip0_12030_13)'>
+        <path d='M7.99 15H20V17H7.99V20L4 16L7.99 12V15Z' fill='#11B3FE' />
+        <path d='M16.01 6H4V8H16.01V11L20 7L16.01 3V6Z' fill='#11B3FE' />
       </g>
       <defs>
-        <clipPath id='clip0_3_98'>
+        <clipPath id='clip0_12030_13'>
           <rect width={24} height={24} fill='white' />
         </clipPath>
       </defs>
