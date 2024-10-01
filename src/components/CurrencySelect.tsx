d5a4e23d98feedc3ac3ff59bf79531139c9a2fe5
Max <mkorobov2002@mail.ru>
2024-10-01 19:43:52 +0300

    Write select, search and open/close logic in CurrencySelect

--- src/components/CurrencySelect.tsx
+++ src/components/CurrencySelect.tsx
@@ -1,59 +1,107 @@
-import Image from 'next/image';
-import { Input } from './Input';
-import { ArrowDown } from './ArrowDown';
+'use client';
 
-const coins = [
-  {
-    ticker: 'btc',
-    name: 'Bitcoin',
-    image: 'https://content-api.changenow.io/uploads/btc_1_527dc9ec3c.svg',
-  },
-  {
-    ticker: 'eth',
-    name: 'Ethereum',
-    image: 'https://content-api.changenow.io/uploads/eth_f4ebb54ec0.svg',
-  },
-  {
-    ticker: 'ethbsc',
-    name: 'Ethereum (Binance Smart Chain)',
-    image: 'https://content-api.changenow.io/uploads/ethbsc_ef444521c5.svg',
-  },
-];
+import Image from 'next/image';
+import { ArrowDown } from './ArrowDown';
+import { useRef, useState } from 'react';
+import { cn } from '@/lib/utils';
+import { testCoins } from '@/temp';
+import { useClickOutside } from '@/hooks';
 
 export const CurrencySelect = () => {
+  const [amount, setAmount] = useState('');
+  const [search, setSearch] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState('btc');
+  const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const selectRef = useClickOutside<HTMLInputElement>(() => {
+    setIsOpen(false);
+  });
+
+  console.log(selectedCurrency);
+
+  const filteredCoins = testCoins.filter(
+    coin =>
+      coin.name.toLowerCase().includes(search.toLowerCase()) ||
+      coin.ticker.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isOpen) {
+      setAmount(e.target.value);
+    } else {
+      setSearch(e.target.value);
+    }
+  };
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+    inputRef.current?.focus();
+  };
+
+  const handleSelect = (ticker: string) => {
+    setSelectedCurrency(ticker);
+    setIsOpen(false);
+  };
+
   return (
-    <div className='relative w-full'>
-      <div className='flex'>
-        <Input type='number' min={0} name='from' />
-        <div className='flex w-[150px] shrink-0 items-center bg-[#f6f7f8] py-[13px] pl-[34px] pr-2'>
-          <Image
-            src={coins[0].image}
-            alt={coins[0].name}
-            width={20}
-            height={20}
-          />
-          <div className='ml-3 mr-[30px] text-base font-normal leading-[23px]'>
-            {coins[0].ticker.toUpperCase()}
-          </div>
-          <ArrowDown />
-        </div>
-      </div>
-      <div className='absolute w-full overflow-hidden rounded-bl-[5px] rounded-br-[5px] border border-[#c1d9e5] border-t-transparent bg-[#f6f7f8]'>
-        {coins.map(coin => (
+    <div ref={selectRef} className='relative w-full'>
+      <div
+        className={cn(
+          'flex rounded-[5px] border border-[#e3ebef] bg-[#f6f7f8]',
+          {
+            'rounded-ee-none rounded-es-none border-l-[#c1d9e5] border-r-[#c1d9e5] border-t-[#c1d9e5]':
+              isOpen,
+          }
+        )}
+      >
+        <input
+          value={!isOpen ? amount : search}
+          onChange={handleChange}
+          ref={inputRef}
+          type={isOpen ? 'text' : 'number'}
+          min={0}
+          name='from'
+          className='w-full bg-transparent px-4 pb-[13px] pt-[14px] text-base font-normal leading-[23px] text-dark-gray outline-none'
+        />
+        {!isOpen && (
           <div
-            key={coin.ticker}
-            className='flex cursor-pointer px-4 py-3 hover:bg-[#eaf1f7]'
+            onClick={handleClick}
+            className='relative flex w-[150px] shrink-0 cursor-pointer items-center py-[13px] pl-[34px] pr-2 before:absolute before:left-0 before:h-[calc(100%-20px)] before:w-[1px] before:bg-[#e3ebef] before:content-[""]'
           >
-            <Image src={coin.image} alt={coin.name} width={20} height={20} />
-            <div className='ml-3 mr-4 text-base font-normal uppercase leading-[23px] text-dark-gray'>
-              {coin.ticker}
+            <Image
+              src={testCoins[0].image}
+              alt={testCoins[0].name}
+              width={20}
+              height={20}
+            />
+            <div className='ml-3 mr-[30px] text-base font-normal leading-[23px]'>
+              {testCoins[0].ticker.toUpperCase()}
             </div>
-            <div className='text-base font-normal leading-[23px] text-[#80a2b6]'>
-              {coin.name}
-            </div>
+            <ArrowDown />
           </div>
-        ))}
+        )}
       </div>
+
+      {isOpen && (
+        <ul className='absolute z-10 max-h-[300px] w-full overflow-hidden overflow-y-auto rounded-bl-[5px] rounded-br-[5px] border border-[#c1d9e5] border-t-transparent bg-[#f6f7f8]'>
+          {filteredCoins.map(coin => (
+            <li
+              key={coin.ticker}
+              className='flex cursor-pointer items-center px-4 py-3 hover:bg-[#eaf1f7]'
+              title={coin.name}
+              onClick={() => handleSelect(coin.ticker)}
+            >
+              <Image src={coin.image} alt={coin.name} width={20} height={20} />
+              <div className='ml-3 mr-4 text-base font-normal uppercase leading-[23px] text-dark-gray'>
+                {coin.ticker}
+              </div>
+              <div className='truncate text-base font-normal leading-[23px] text-[#80a2b6]'>
+                {coin.name}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
