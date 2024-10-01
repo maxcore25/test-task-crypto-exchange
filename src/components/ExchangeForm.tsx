bd6c41ed12a67a3ee03c72714379410b9d73e430
Max <mkorobov2002@mail.ru>
2024-10-01 20:40:38 +0300

    Pass props into CurrencySelect in ExchangeForm

--- src/components/ExchangeForm.tsx
+++ src/components/ExchangeForm.tsx
@@ -1,16 +1,20 @@
+import { FormEvent } from 'react';
 import { Button } from './Button';
 import { CurrencySelect } from './CurrencySelect';
 import { Input } from './Input';
 import { SwapButton } from './SwapButton';
 
 export const ExchangeForm = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='mb-12 flex flex-col gap-4 lg:mb-8 lg:flex-row lg:items-center lg:gap-7'>
-        {/* <Input type='number' min={0} name='from' /> */}
-        <CurrencySelect />
+        <CurrencySelect id='from' name='from' defaultCurrency='btc' />
         <SwapButton className='rotate-90 self-end lg:rotate-0 lg:self-auto' />
-        <Input type='number' min={0} name='to' />
+        <CurrencySelect id='from' name='from' defaultCurrency='eth' />
       </div>
       <div className='grid gap-4 lg:flex lg:items-end lg:gap-8'>
         <div className='grid w-full gap-2'>
