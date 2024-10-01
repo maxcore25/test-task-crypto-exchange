0a7939625852fc6a7d1c228e02f1a03312af0c8f
Max <mkorobov2002@mail.ru>
2024-10-02 02:41:13 +0300

    Export hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,21 +1,2 @@
-import { useEffect, useRef } from 'react';
-
-export function useClickOutside<T extends HTMLElement>(handler: () => void) {
-  const ref = useRef<T>(null);
-
-  const handleClickOutside = (e: MouseEvent) => {
-    if (ref.current && !ref.current.contains(e.target as Node)) {
-      handler();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside, true);
-
-    return () => {
-      document.removeEventListener('click', handleClickOutside, true);
-    };
-  });
-
-  return ref;
-}
+export * from './useClickOutside';
+export * from './useDebounce';
