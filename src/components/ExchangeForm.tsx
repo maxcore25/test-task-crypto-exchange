8010337e697b37626c08d8870245ddae14496c5d
Max <mkorobov2002@mail.ru>
2024-10-01 20:53:38 +0300

    Change id and name props in the second CurrencySelect

--- src/components/ExchangeForm.tsx
+++ src/components/ExchangeForm.tsx
@@ -15,7 +15,7 @@ export const ExchangeForm = () => {
       <div className='mb-12 flex flex-col gap-4 lg:mb-8 lg:flex-row lg:items-center lg:gap-7'>
         <CurrencySelect id='from' name='from' defaultCurrency={btcCoin} />
         <SwapButton className='rotate-90 self-end lg:rotate-0 lg:self-auto' />
-        <CurrencySelect id='from' name='from' defaultCurrency={ethCoin} />
+        <CurrencySelect id='to' name='to' defaultCurrency={ethCoin} />
       </div>
       <div className='grid gap-4 lg:flex lg:items-end lg:gap-8'>
         <div className='grid w-full gap-2'>
