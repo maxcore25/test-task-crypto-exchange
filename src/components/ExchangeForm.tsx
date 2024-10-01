06940be6ce4b5aecc5c19cfa24364194ca2a3942
Max <mkorobov2002@mail.ru>
2024-10-01 22:13:56 +0300

    Fetch min amount

--- src/components/ExchangeForm.tsx
+++ src/components/ExchangeForm.tsx
@@ -1,11 +1,36 @@
-import { FormEvent } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { Button } from './Button';
 import { CurrencySelect } from './CurrencySelect';
 import { Input } from './Input';
 import { SwapButton } from './SwapButton';
 import { btcCoin, ethCoin } from '@/temp';
+import { Currency, MinAmount } from '@/types';
+import { axiosInstance } from '@/api';
 
 export const ExchangeForm = () => {
+  const [fromCurrencySelect, setFromCurrencySelect] =
+    useState<Currency>(btcCoin);
+  const [toCurrencySelect, setToCurrencySelect] = useState<Currency>(ethCoin);
+  const [fromCurrencyAmount, setFromCurrencyAmount] = useState(0);
+  // const [toCurrencyAmount, setToCurrencyAmount] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(
+        'Sending request for:',
+        fromCurrencySelect.ticker,
+        toCurrencySelect.ticker
+      );
+
+      const res = await axiosInstance.get<MinAmount>(
+        `/min-amount/${fromCurrencySelect.ticker}_${toCurrencySelect.ticker}?api_key=${import.meta.env.VITE_API_KEY}`
+      );
+      setFromCurrencyAmount(res.data.minAmount);
+    };
+
+    fetchData();
+  }, [fromCurrencySelect, toCurrencySelect]);
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
@@ -13,9 +38,20 @@ export const ExchangeForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div className='mb-12 flex flex-col gap-4 lg:mb-8 lg:flex-row lg:items-center lg:gap-7'>
-        <CurrencySelect id='from' name='from' defaultCurrency={btcCoin} />
+        <CurrencySelect
+          id='from'
+          name='from'
+          minAmount={fromCurrencyAmount}
+          selectedCurrency={fromCurrencySelect}
+          onCurrencyChange={setFromCurrencySelect}
+        />
         <SwapButton className='rotate-90 self-end lg:rotate-0 lg:self-auto' />
-        <CurrencySelect id='to' name='to' defaultCurrency={ethCoin} />
+        <CurrencySelect
+          id='to'
+          name='to'
+          selectedCurrency={toCurrencySelect}
+          onCurrencyChange={setToCurrencySelect}
+        />
       </div>
       <div className='grid gap-4 lg:flex lg:items-end lg:gap-8'>
         <div className='grid w-full gap-2'>
