1fdbbf05fd38714fa6008bc5bd2fa9651d999cc3
Max <mkorobov2002@mail.ru>
2024-10-02 14:57:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  define: {
-    VITE_API_KEY: process.env.VITE_API_KEY,
-  },
+    define: {
+      VITE_API_KEY: JSON.stringify(env.VITE_API_KEY),
+      // 'process.env': env,
+    },
+  };
 });
